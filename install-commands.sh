54d3b8623315fd3774701f8eb76b3998c6f82383
GuyBarGil <g.bargil@gmail.com>
2019-11-24 11:53:38 +0200

    Update install-commands.sh

--- install-commands.sh
+++ install-commands.sh
@@ -1,3 +1,2 @@
 # Install Node.js (8.9.4) + NPM (5.6.0)
-RUN curl -sL https://deb.nodesource.com/setup_8.x | bash && \
-    apt-get install -y nodejs build-essential
+curl -sL https://deb.nodesource.com/setup_8.x | bash && apt-get install -y nodejs build-essential
